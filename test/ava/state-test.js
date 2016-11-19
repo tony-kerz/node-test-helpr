162e3c8ed654c03e305aa16b2dac757186dc47bd
Tony L. Kerz <anthony.kerz@gmail.com>
2016-11-19 09:58:26 -0500

    state

---
+++ test/ava/state-test.js
@@ -0,0 +1,45 @@
+import test from 'ava'
+import {initState, getState, getRequiredState, setState, unsetState} from '../../src'
+
+test.beforeEach(async ()=>{
+  initState()
+})
+
+test('initState', t => {
+  setState({foo: 'bar'})
+  initState()
+  t.deepEqual(getState(), {})
+})
+
+test('setState', t => {
+  const state = {foo: 'bar'}
+  setState(state)
+  t.deepEqual(getState(), state)
+})
+
+test('getState', t => {
+  const state = {foo: 'bar'}
+  setState(state)
+  t.deepEqual(getState('foo'), state.foo)
+})
+
+test('getState: default', t => {
+  const state = {foo: 'bar'}
+  setState(state)
+  const dflt = 'baz'
+  t.deepEqual(getState('bar', {dflt}), dflt)
+})
+
+test('getRequiredState', t => {
+  const state = {foo: 'bar'}
+  setState(state)
+  t.deepEqual(getRequiredState('foo'), state.foo)
+  t.throws(()=>{getRequiredState('bar')})
+})
+
+test('unsetState', t => {
+  const state = {foo: 'bar'}
+  setState(state)
+  unsetState('foo')
+  t.deepEqual(getState(), {})
+})
